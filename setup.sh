ddf67b7c2cb9bbaa1c6cadf970fec65dd3eda9a8
jieli <jieli@boxer.lan>
2022-08-03 18:00:34 -0400

    avoid use less in git output

--- setup.sh
+++ setup.sh
@@ -17,15 +17,14 @@ brew_install(){
 update_profile(){
     prompt---- "update .zshrc"
     cp  ~/.zshrc ~/.zshrc.$(date +%Y%m%d.%H%M%S)
-    echo "" > ~/.zshrc 
-    cat >> ~/.zshrc <<EOF
-. /Users/jieli/.workenv/profile/zshrc
-. /Users/jieli/.workenv/profile/commonrc
-EOF
+    echo ". ~/.workenv/profile/commonrc" >> ~/.zshrc
+    echo ". ~/.workenv/profile/zshrc" > ~/.zshrc 
+    # move to inside commonrc
+    #echo '[ -f ~/.customrc ] && . ~/.customrc' >> ~/.zshrc
 }
 
-mac_install(){
-
+mac_preinstall(){
+    prompt---- " preinstall manual config mac"
     ###################### manual presteps ############################
     # * create a "brewer" admin user
     # * rename computer
@@ -36,37 +35,46 @@ mac_install(){
     #    pbcopy <id_rsa.pub
     # * install  chrome
     ####################### common #############################
-
     prompt---- " preinstall xcode-select"
     # xcode-select --install
+}
 
-    prompt---- "homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+zsh_and_theme(){
+    prompt---- "zsh ohmyzsh & theme" 
+    [ ! -d $ZSH ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]  && \
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+}
 
-
-    prompt---- "sdkman"
-    curl -s https://get.sdkman.io | bash
-    source "/Users/jieli/.sdkman/bin/sdkman-init.sh"
-
+brew_app_minimal(){
     brew_install node 
     brew_install yarn
     brew_install yq 
     brew_install ansible
     brew_install --cask intellij-idea
     brew_install iterm2    
+    brew_install tree
+}
+
+mac_install(){
+
+    zsh_and_theme
+
+    prompt---- "homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    prompt---- "sdkman"
+    curl -s https://get.sdkman.io | bash
+    source "/Users/jieli/.sdkman/bin/sdkman-init.sh"
+
+    brew_app_minimal
 
     prompt---- "vim"
     . vim/setenv.sh
 
     prompt---- "java"
-    sdk install java
-
-    prompt---- "zsh ohmyzsh & theme" 
-    [ ! -d $ZSH ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]  && \
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
+    sdk install java 18.0.2-amzn 
 
     update_profile
 
@@ -83,14 +91,13 @@ mac_uninstall(){
     done
 
 
-    # uninstall homebrew
+    #prompt--- "uninstall homebrew"
     # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
     # sudo rm -rf /opt/homebrew
 
-    # uninstall sdkman
+    #prompt---- "uninstall sdkman"
     # rm -rf ~/.sdkman
     # then remove from all profiles
-
 }
 
 
@@ -110,6 +117,7 @@ case $1 in
     mac) 
         case $2 in
             install)
+                mac_preinstall
                 mac_install
                 ;;
             uninstall)
