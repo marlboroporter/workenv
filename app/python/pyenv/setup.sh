ab221fd7560a1f4ec9de468efb5139cdd146847a
jieli <jieli@boxer.lan>
2022-08-26 02:50:47 -0400

    ok

--- app/python/pyenv/setup.sh
+++ app/python/pyenv/setup.sh
@@ -1,18 +1,7 @@
 install(){
 brew install pyenv 
-#pyenv install 3.10.6 # path and init set in commonrc
-#pyenv global 3.10.6 # python point to this
-eval "$(setenv)"
-
+#eval "$(setenv)"
 }
-
-#setenv(){
-#cat<<EOF
-#export PYENV_ROOT="\$HOME/.pyenv"
-#command -v pyenv >/dev/null || export PATH="\$PYENV_ROOT/bin:\$PATH"
-#eval "\$(pyenv init -)"
-#EOF
-#}
 
 setenv(){
 echo 'export PYENV_ROOT="$HOME/.pyenv"
@@ -20,28 +9,37 @@ command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"'
 }
 
-nvim_config(){
+setenv_pyenv_py(){
   eval "$(setenv)"
-  # - A
-  # this offer shim env, such as python and pip alias based on set version   
-  #pyenv install 3.10.6 # path and init set in commonrc
-  #pyenv global 3.10.6 # python point to this
-  
-  # - B
-  # use system python and pip
-  # so the pip and python alias needed before pip install further
-  # need source python/setup.sh#setenv()
-  pyenv global system # this does not offer shim env such as pip alias from shims nor python alias
-  
+  pyenv install 3.10.6 # path and init set in commonrc
+  pyenv global 3.10.6 # python point to this
+}
+ 
+setenv_system_py(){
   # this goes to system python3 env or pyenv depends on pyenv set 
+  # pyenv global system 
+  #   does not offer shim env such as pip alias from shims nor python alias
+  alias pip='/usr/bin/pip3'
+  alias python='/usr/bin/python3'
+}
+  
+do_config(){
   pip install pynvim # install to current python verion dir
   pip install 'python-lsp-server[all]' pylsp-mypy pyls-isort
-  brew install ripgrep
+  #brew install ripgrep
   pip install pylint
   pip install flake8
   pip install vint
 }
 
 config(){
-    nvim_config
+
+  # somehow still not working together when pyenv setup
+  #setenv_system_py
+  #do_config
+
+  # now stick to pyenv setup
+  setenv_pyenv_py
+  do_config
 }
+
--- app/python/setup.sh
+++ app/python/setup.sh
@@ -1,9 +1,7 @@
 install(){
     : # use system python3
 }
-
-
-setenv(){
+setenv_not_used(){
 cat<<EOF
 # python setting without pyenv nor brew installed python
 # pyenv config depends on this to run  if no new pyenv versions installed and set
@@ -12,7 +10,9 @@ alias pip='/usr/bin/pip3'
 alias python='/usr/bin/python3'
 EOF
 }
-
+setenv(){
+    : #noop 
+}
 config(){
     : #noop
 }
