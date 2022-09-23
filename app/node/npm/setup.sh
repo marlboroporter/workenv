c80f8e939d740fdcd911876b7930fa818ef8a2d0
jieli <jieli@boxer.lan>
2022-09-23 15:55:23 -0400

    spacevim

--- app/node/npm/setup.sh
+++ app/node/npm/setup.sh
@@ -1,3 +1,10 @@
+pkgmgr=brew
 config(){
+ # show move to vim
  npm install -g vim-language-server
+}
+
+info(){
+  echo "Current global npm modules: at $(npm root -g): "
+  ls -1 $(npm root -g ) 
 }
