660b2fee9a0743e91cf118c123e77d3efb9093be
jieli <jieli@boxer.lan>
2022-09-27 19:50:40 -0400

    cleanup

--- app/docker/setup.sh
+++ app/docker/setup.sh
@@ -1,3 +1,33 @@
+
+
+
 install(){
   echo "download Docker.dmg from docker.com"
+
+  # auto install
+  sudo hdiutil attach Docker.dmg
+  sudo /Volumes/Docker/Docker.app/Contents/MacOS/install \
+    --accept-license \
+    --user=jieli
+  sudo hdiutil detach /Volumes/Docker
+
+  # for intel chip app to run on M1 
+  softwareupdate --install-rosetta
 }
+
+uninstall(){
+   /Applications/Docker.app/Contents/MacOS/Docker --uninstall
+}
+
+
+tutorial(){
+  # M1 specific
+  # https://docs.docker.com/desktop/mac/apple-silicon/
+  # tutorial
+  # https://docs.docker.com/get-started/      
+   docker run -d -p 80:80 docker/getting-started
+}
+
+
+
+
--- setup.sh
+++
@@ -1,1 +0,0 @@
-pkgmgr=brew
