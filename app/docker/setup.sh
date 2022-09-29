7b7ea854eb8b0fa1f3e68d0dba99a07a329aedfd
jieli <jieli@boxer.lan>
2022-09-29 14:00:36 -0400

    docker

--- app/docker/setup.sh
+++ app/docker/setup.sh
@@ -1,9 +1,6 @@
-
-
-
 install(){
-  echo "download Docker.dmg from docker.com"
-
+  #echo "download Docker.dmg from docker.com"
+  wget "https://desktop.docker.com/mac/main/arm64/Docker.dmg?utm_source=docker&utm_medium=webreferral&utm_campaign=dd-smartbutton&utm_location=module" -O ~/Downloads/Docker.img
   # auto install
   sudo hdiutil attach Docker.dmg
   sudo /Volumes/Docker/Docker.app/Contents/MacOS/install \
