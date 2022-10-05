b5ffec221ac68e7bdc42b74a3ff16232e1a1d51e
jieli <jieli@boxer.lan>
2022-10-04 21:54:25 -0400

    e update

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Install
-* clone to ~/.e; app/* unique unless link 
-* optional
-* mkdir -p ~/.w; work 
-* mkdir -p ~/.p; private 
-
+#export EROOT
--- lib/commands.sh
+++ lib/commands.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env zsh
 . ~/.e/etc/env.sh
+
+
 unalias g
-alias g='go_app_default_in_e'
+alias g='bt_go_app_default_in_e'
+alias ereplace='bt_rename_all'
+
 
 # params: appname, eroot
-go_app_default_in_e() {
+bt_go_app_default_in_e() {
   app=$1
   eroot=$2
   if [[ "$eroot" == "" ]]; then
@@ -38,4 +42,43 @@ go_app_default_in_e() {
   
 }
 
+bt_get_AllLinks(){
+  echo "--------Link in .e/app"
+  find ~/.e/app -type l  |while read x; do ls -l $x; done
+}
 
+bt_get_all_apps(){
+  prefix=~/.e/app
+  suffix=setup.sh
+  D=$(find ~/.e/app -type d)
+  for d in $D 
+  do
+    [ -f $d/setup.sh ] && echo ${d#"$prefix/"}
+  done
+}
+
+bt_go_to_app(){
+  prefix=~/.e/app
+  cd "$prefix/$(getApp $1)"
+}
+
+
+# usage
+# rename_all p old new
+# rename_all w old new
+# rename_all e old new
+bt_rename_all() {
+  envroot=$1 
+  old=$2
+  new=$3
+  dirs=($(find ~/.$envroot/ -type d |grep -v "\.git"))
+  for d in "${dirs[@]}"
+  do
+    files=($(find $d  -type f -name "*.sh")) 
+    for f in  "${files[@]}" 
+    do
+      echo $f
+      sed -i '' -e "s/$old/$new/g" "$f"  
+    done
+  done
+}
