4fe3b59bb35ae286c7f63f0442606eb8de23facf
jieli <jieli@boxer.lan>
2022-10-04 23:46:13 -0400

    correct

--- app/ohmyzsh/setup.sh
+++ app/ohmyzsh/setup.sh
@@ -1,8 +1,8 @@
 pkgmgr=noop
 install(){
 echo "$info"
-[ ! -d $ZSH ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-[ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k]  && \
+[[ ! -d $ZSH ]] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[[ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]]  && \
     git clone --depth=1 https://github.com/romkatv/poweerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
 reinstall(){
