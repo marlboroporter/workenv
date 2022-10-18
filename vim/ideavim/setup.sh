0c55cd9a8998962f3178a56fe79218b83c8becca
jieli <jieli@boxer.lan>
2022-10-18 10:06:35 -0400

    ideavim

--- vim/ideavim/setup.sh
+++ vim/ideavim/setup.sh
@@ -1,7 +1,7 @@
 pkgmgr=noop
 config(){
     #
-    ln -sf ~/.e/app/vim/ideavim/ideavimrc ~/.ideavimrc
+    ln -sf $PWD/ideavimrc ~/.ideavimrc
     # j,k key press to repeat     
-    defaults.erite -g ApplePressAndHoldEnabled -bool false
+    defaults write -g ApplePressAndHoldEnabled -bool false
 }
