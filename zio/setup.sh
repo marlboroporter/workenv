2e379bd205b947013cfdfdf58ab8bbd354ccba1c
jieli <jieli@boxer.lan>
2022-11-03 13:34:23 -0400

    clone clean

--- zio/setup.sh
+++ zio/setup.sh
@@ -1,4 +1,4 @@
 samples(){
-x_clone https://github.com/zio/zio-json.git
-x_clone https://github.com/milessabin/shapeless.git
+x_git_clone https://github.com/zio/zio-json.git
+x_git_clone https://github.com/milessabin/shapeless.git
 }
