565d0a6e3cc5e4c1fe3568d26948ed640f77aba5
jieli <jieli@boxer.lan>
2023-01-05 11:57:04 -0500

    cpp simple

---
+++ vscode/etc/cpp_ex_2/c.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2022 jieli <jieli@boxer.lan>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   int x=10;
+   printf("Hello, World!");
+   return 0;
+}
+
+
+
+
