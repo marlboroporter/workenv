5ae6a2204b5ac18a0c8c721eb79c3f04125c3118
jieli <jieli@boxer.lan>
2023-01-16 20:01:01 -0500

    container

---
+++ vscode/devcontainer/README.md
@@ -0,0 +1,4 @@
+# Dev Container
+* [VsCode Devcontainer](https://code.visualstudio.com/docs/devcontainers/containers) 
+* [Development Containers Specification](https://containers.dev/)
+* [tutorial](https://code.visualstudio.com/docs/devcontainers/tutorial)
---
+++ vscode/devcontainer/setup.sh
@@ -0,0 +1,2 @@
+# [Dev Containers](https://code.visualstudio.com/docs/devcontainers/containers)
+# [open in dev container](https://vscode.dev/redirect?url=vscode://ms-vscode-remote.remote-containers/cloneInVolume?url=https://github.com/microsoft/vscode-remote-try-python) 
