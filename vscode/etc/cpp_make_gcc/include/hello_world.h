7288a3062058f469a89dfcfc49a95f24f94e014a
jieli <jieli@boxer.lan>
2023-01-23 16:14:12 -0500

    cpp make gcc

---
+++ vscode/etc/cpp_make_gcc/include/hello_world.h
@@ -0,0 +1,7 @@
+#ifndef RU_SOE_DA_HELLOWOLRLD_H
+#define RU_SOE_DA_HELLOWOLRLD_H
+namespace da537 {
+    int say_hello();
+
+}
+#endif
---
+++ vscode/etc/cpp_make_gcc/src/client.cpp
@@ -0,0 +1,6 @@
+#include "hello_world.h"
+
+int main(){
+    da537::say_hello();   
+    return 0;
+}
