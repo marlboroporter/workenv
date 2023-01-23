614895e9af6086d930987bc953dd4e96761cd54d
jieli <jieli@boxer.lan>
2023-01-23 16:17:56 -0500

    makefile

---
+++ lang/cpp/clang/llvm/README.md
@@ -0,0 +1,2 @@
+# this is not working yet
+# installed separated, may conflict with xcode ?
---
+++ leetcode/setup.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# setup.sh
+# Copyright (C) 2023 jieli <jieli@boxer.lan>
+#
+# Distributed under terms of the MIT license.
+#
+setenv="
+export PATH=\$PATH:$PWD/bin
+"
+
+
