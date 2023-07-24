baaae0f95e337fed6043622466ef64df143d491b
jieli <jieli@boxer.lan>
2023-07-24 18:16:13 -0400

    screen shot

--- mac/preference/setup.sh
+++ mac/preference/setup.sh
@@ -12,3 +12,12 @@ defaults write com.apple.finder CreateDesktop -bool false
 # apple script to all.e to press key to repeat 
 defaults write -g ApplePressAndHoldEnabled -bool false
 "
+install (){
+# apple script to hide screenshot etc icon  on desktop
+defaults write com.apple.finder CreateDesktop -bool false
+# apple script to all.e to press key to repeat 
+defaults write -g ApplePressAndHoldEnabled -bool false
+# screenshot name default
+defaults write com.apple.screencapture name "Screenshot"
+
+}
