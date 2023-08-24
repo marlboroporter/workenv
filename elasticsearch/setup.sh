90a86a8c018d9058644ce8d28f902e61b7f0af8e
jieli <jieli@boxer.lan>
2023-08-24 18:24:15 -0400

    elastics

--- elasticsearch/setup.sh
+++ elasticsearch/setup.sh
@@ -1,3 +1,12 @@
 install(){
     docker pull docker.elastic.co/elasticsearch/elasticsearch:8.9.1
 }
+
+config(){
+    # create a docker network
+    docker network create elastic
+    # Start Elasticsearch in Docker 
+    docker run --name es01 --net elastic -p 9200:9200 -it docker.elastic.co/elasticsearch/elasticsearch:8.9.1
+    # 
+}
+
