b0558fd6f1ea1389c523adcf94ee3da99cbe02c1
jieli <jieli@boxer.lan>
2024-02-04 09:20:52 -0500

    remove conda

--- lang/python/pyenv/setup.sh
+++ lang/python/pyenv/setup.sh
@@ -3,8 +3,7 @@ install(){
     brew reinstall pyenv 
     brew reinstall  pyenv-virtualenv
 }
-xappdef="pyeny & conda"
-
+xappdef="pyeny"
 
 setenv='
 export PYENV_ROOT="$HOME/.pyenv"
@@ -13,14 +12,7 @@ eval "$(pyenv init -)"
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 '
-#config_nvim(){
-#  pip install pynvim # install to current python verion dir
-#  pip install 'python-lsp-server[all]' pylsp-mypy pyls-isort
-#  #brew install ripgrep
-#  pip install pylint
-#  pip install flake8
-#  pip install vint
-#}
+
 
 config_default_python(){
     eval "$setenv"
@@ -31,11 +23,8 @@ config_default_python(){
 config(){
   eval "$setenv"
   config_default_python
-  #config_nvim
 }
 
-info(){
-    echo "# pyenv global 3.12"
-}
-
-# pyenv pypi does not support M1 chip, use conda
+# History
+#*  pyenv pypi does not support M1 chip, use conda
+#*  2024 hope pyenv pypi support M1 chip
